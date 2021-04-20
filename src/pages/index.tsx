9cab0438b32851775cc0faebf3dae97170da7339
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-20 19:29:27 -0300

    Example getStaticProps - SSG(static site generation)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,14 @@ export default function Home(props) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const response = await fetch('http://localhost:3333/episodes')
   const data = await response.json()
 
   return {
     props: {
       episodes: data
-    }
+    },
+    revalidate: 60 * 60 * 8 // Recebe um número em segundos que significa de quanto em quanto tempo eu quero gerar uma nova versão dessa página
   }
 }
