2088e3c4a6636e5b859cbbcf6ad55bcf00d46967
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-20 19:22:05 -0300

    Example getServerSideProps - SSR(server-side rendering)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes)
+
   return (
-    <h1>Index</h1>
+    <>
+      <h1>Index</h1>
+      <p>{ JSON.stringify(props.episodes) }</p>
+    </>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    }
+  }
 }
