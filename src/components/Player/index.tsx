88b73f6f2a241ac19b87eae209175b5d6b09839d
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-23 11:57:08 -0300

    Showing informations in the player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react'
-import { PlayerContext } from '../../contexts/PlayerContext'
+import Image from 'next/image'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
 
+import { PlayerContext } from '../../contexts/PlayerContext'
 import styles from './styles.module.scss'
 
 export function Player() {
@@ -12,36 +15,59 @@ export function Player() {
     <div className={ styles.playerContainer }>
       <header>
         <img src="/playing.svg" alt="Tocando agora"/>
-        <strong>Tocando agora { episode?.title }</strong>
+        <strong>Tocando agora</strong>
       </header>
 
-      <div className={ styles.emptyPlayer }>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={ styles.currentEpisode }>
+          <Image
+            width={592}
+            height={592}
+            src={ episode.thumbnail }
+            objectFit="cover"
+          />
+          <strong>{ episode.title }</strong>
+          <span>{ episode.members }</span>
+        </div>
+      ) : (
+        <div className={ styles.emptyPlayer }>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
 
-      <footer className={ styles.empty }>
+      <footer className={ !episode ? styles.empty : '' }>
         <div className={ styles.progress }>
           <span>00:00</span>
+
           <div className={ styles.slider }>
-            <div className={ styles.emptySlider } />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#04d361' }} // Vai mudar a cor do progresso que já aconteceu
+                railStyle={{ backgroundColor: '#9f75ff' }} // Cor de fundo da barra que ainda não sofreu progresso
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }} // Vai alterar a bolinha do slider
+              />
+            ) : (
+              <div className={ styles.emptySlider } />
+            )}
           </div>
+
           <span>00:00</span>
         </div>
 
         <div className={ styles.buttons }>
-          <button type="button">
+          <button type="button" disabled={ !episode }>
             <img src="/shuffle.svg" alt="Embaralhar"/>
           </button>
-          <button type="button">
+          <button type="button" disabled={ !episode }>
             <img src="/play-previous.svg" alt="Tocar anterior"/>
           </button>
-          <button type="button" className={ styles.playButton }>
+          <button type="button" className={ styles.playButton } disabled={ !episode }>
             <img src="/play.svg" alt="Tocar"/>
           </button>
-          <button type="button">
+          <button type="button" disabled={ !episode }>
             <img src="/play-next.svg" alt="Tocar próxima"/>
           </button>
-          <button type="button">
+          <button type="button" disabled={ !episode }>
             <img src="/repeat.svg" alt="Repetir"/>
           </button>
         </div>
