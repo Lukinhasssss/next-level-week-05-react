22f32cbf50865d92b00358be1560331aa861166a
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-23 16:48:31 -0300

    Application finished

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import Head from 'next/head'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
@@ -30,6 +32,10 @@ export default function Episode({ episode }: EpisodeProps) {
 
   return (
     <div className={ styles.episode }>
+      <Head>
+        <title>{ episode.title } | Podcastr</title>
+      </Head>
+
       <div className={ styles.thumbnailContainer }>
         <Link href="/">
           <button type="button">
