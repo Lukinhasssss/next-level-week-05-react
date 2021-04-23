95bae50da317791f89ef9e2a639bad6f2c963cd6
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-23 10:31:12 -0300

    Simple example of context

---
+++ src/contexts/PlayerContext.ts
@@ -0,0 +1,3 @@
+import { createContext } from 'react'
+
+export const PlayerContext = createContext('') // createContext recebe o formato dos dados que eu quero salvar, por exemplo: string'', objeto{}, array[]
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,19 @@ import { Header } from '../components/Header'
 import { Player } from '../components/Player'
 
 import styles from '../styles/app.module.scss'
+import { PlayerContext } from '../contexts/PlayerContext'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={ styles.wrapper }>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={'Hotel California'}>
+      <div className={ styles.wrapper }>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
