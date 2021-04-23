88f6b9e7e26b82c86cf1166b3cbf622bb4e38e7c
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-23 11:17:33 -0300

    Add player context

--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -1,3 +1,17 @@
 import { createContext } from 'react'
 
-export const PlayerContext = createContext('') // createContext recebe o formato dos dados que eu quero salvar, por exemplo: string'', objeto{}, array[]
+type Episode = {
+  title: string
+  members: string
+  thumbnail: string
+  duration: number
+  url: string
+}
+
+type PlayerContextData = {
+  episodeList: Episode[]
+  currentEpisodeIndex: number
+  play: (episode: Episode) => void
+}
+
+export const PlayerContext = createContext({} as PlayerContextData) // createContext recebe o formato dos dados que o context vai iniciar, por exemplo: string'', objeto{}, array[]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
-import { api } from '../services/api'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import Link from 'next/link'
+import { useContext } from 'react'
 
+import { api } from '../services/api'
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
+import { PlayerContext } from '../contexts/PlayerContext'
+
 import styles from './home.module.scss'
 
 type Episode = {
@@ -25,6 +28,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
+
   return (
     <div className={ styles.homepage }>
       <section className={ styles.latestEpisodes }>
@@ -51,7 +56,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{ episode.durationAsString }</span>
                 </div> 
 
-                <button>
+                <button type="button" onClick={ () => play(episode) }>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
